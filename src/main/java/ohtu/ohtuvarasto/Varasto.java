4f284a853b6b5e75ec3c22888ab7825d549e632e
tnli <tnli@iki.fi>
2020-11-09 15:33:28 +0200

    tehdään virhe

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -57,7 +57,8 @@ public class Varasto {
     }
 
     public double otaVarastosta(double maara) {
-        if (maara < 0) { // virhetilanteessa voidaan tehdä 
+        if (maara < 0) 
+		{ // virhetilanteessa voidaan tehdä 
             return 0.0;   // tällainen pikapoistuminenkin!
         }
         if (maara > saldo) {          // annetaan mitä voidaan
